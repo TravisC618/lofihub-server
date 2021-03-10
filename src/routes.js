3cecd45714404b06b0e61e34d86f8eaa38ff3d64
travisC <travis_zg@outlook.com>
2021-03-10 19:31:55 +1000

    added co-wechat middleware

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const coWechat = require('co-wechat');
 const userRoute = require("./routes/user");
 const videoRoute = require("./routes/videos");
 const authRoute = require("./routes/auth");
 const wechatRoute = require("./routes/wechat");
 
 const router = express.Router();
+const wechatConf = {
+    appid: 'wx7d05a51683c6aaad',
+    appsecret: '027138ab9ce27183de1b3770d728926f', 
+    token: 'jrtesting',
+}
 
+router.all('/wechat', coWechat(wechatConf).middleware(
+    async message => {
+        console.log('wechart', message)
+        return 'Hello world! ' + message.Content;
+}));
 router.use("/users", userRoute);
 router.use("/videos", videoRoute);
 router.use("/auth", authRoute);
