25776a9b25361cd6d7b93caec3648f994e9edca0
travisC <travis_zg@outlook.com>
2021-03-10 18:05:00 +1000

    async

--- src/controllers/wechat.js
+++ src/controllers/wechat.js
@@ -8,32 +8,25 @@ const { formateResponse } = require("../utils/helper");
 const ACCESS_TOKEN =
     "36_Lh7JKEn4ITYYML-Yb8MKiE6DE0GeAa3wK4PERMjJK_UVFazWLNcf5s1x_sSTVRMbJ2xC_qxbi94PAz451NYqcC5dN3yJKkcxpiPH7YGp2mNkNxfsbLDkkenMrl5tQ7bymfdOjsk94BOX5dIUHUOgAJALYS";
 
-const hashStr = (data) => {
-    const hash = createHash("sha1");
-    hash.update(data);
-    const result = hash.digest("hex");
-    return result;
-};
-
-const checkSignature = (req, res) => {
-    const { signature, timestamp, nonce, echostr } = req.body;
+const checkSignature = async (req, res) => {
     const token = "jrtesting";
-    const tempArr = [token, timestamp, nonce];
-    tempArr.sort();
-    const combinedStr = tempArr.reduce((acc, curr) => {
-        acc += curr;
-        return acc;
-    });
+    const query = urlFormat.parse(req.url, true).query;
+    const { signature, timestamp, nonce, echostr } = query;
 
-    const hashed = hashStr(combinedStr);
-
-    if (hashed === signature) {
+    console.log("isValid", isValid(timestamp, nonce, signature, token));
+    if (isValid(timestamp, nonce, signature, token)) {
         return res.send(echostr);
     }
-    return null;
+    return res.send(false);
 };
 
-const getMessage = (req, res) => {
+const isValid = (timestamp, nonce, signature, token) => {
+    const tmp = [token, timestamp, nonce].sort().join('');
+    const currSign = createHash('sha1').update(tmp).digest('hex');
+    return currSign === signature;
+}
+
+const getMessage = async (req, res) => {
     const { xml: msg } = req.body;
     console.log('Receive:', msg)
     const builder = new xml2js.Builder();
