47cb1a2ffb56133652ec4e799e5956dc71c8f8dc
travisC <travis_zg@outlook.com>
2021-03-10 19:43:05 +1000

    modified routes

--- src/routes/wechat.js
+++ src/routes/wechat.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { checkSignature, evenHandler, getMessage } = require("../controllers//wechat");
 const router = express.Router();
 
-router.get("/", checkSignature);
-router.post("/", getMessage)
+router.get("/test", checkSignature);
+router.post("/test", getMessage)
 router.post("/event", evenHandler);
 
 module.exports = router;
